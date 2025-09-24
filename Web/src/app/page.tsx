9e1d654f7cd9c2ac12290aeec286acf6f495bf00
Takato180 <masymyt@gmail.com>
2025-09-24 17:24:21 +0900

    [fix] DiaryEntryCreateを@/lib/apiからimportに追加

--- Web/src/app/page.tsx
+++ Web/src/app/page.tsx
@@ -14,6 +14,7 @@ import {
   getActivitySuggestions,
   uploadImageFile,
   DiaryEntry,
+  DiaryEntryCreate,
 } from "@/lib/api";
 import { AuthProvider, useAuth } from "@/hooks/useAuth";
 import AuthModal from "@/components/AuthModal";
